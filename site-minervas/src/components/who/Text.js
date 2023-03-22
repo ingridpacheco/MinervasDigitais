0ce3300375accf225f09d17a27513a70ccec1082
Ingrid Pacheco <iq.pacheco23@gmail.com>
2023-03-21 21:09:29 -0300

    feat: Add new component about the group

---
+++ site-minervas/src/components/who/Text.js
@@ -0,0 +1,60 @@
+import React from "react"
+
+import Button from "@material-ui/core/Button"
+import { withStyles } from "@material-ui/core/styles"
+import { redirect } from "react-router-dom"
+
+import "./Text.scss"
+
+const ColorButton = withStyles(() => ({
+  root: {
+    textTransform: "none",
+    borderRadius: 55,
+    fontFamily: "Mulish",
+    fontWeight: "bold",
+    backgroundColor: "#096262",
+    "&:hover": {
+      backgroundColor: "#13BADE",
+      fontWeight: "bold",
+    },
+  },
+}))(Button)
+
+class Text extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      openChat: false
+    }
+  }
+
+  openChat = () => {
+    this.setState({
+      openChat: true
+    })
+  }
+
+  render(){
+    const {click} = this.props
+    console.log(this.props)
+
+    return (
+      <div className="text">
+        <div className="title">
+          Quem somos?
+        </div>
+        <div className="description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mi nunc, laoreet eu neque sit amet, ultricies aliquet libero. Ut tempor pharetra feugiat. Duis et consequat massa, viverra cursus nibh. Suspendisse nisi erat, feugiat sit amet porta quis, egestas quis nisl. Ut fermentum mi in eros porttitor molestie. Cras justo massa, luctus in enim eu, imperdiet convallis massa. Aenean dictum sem eget lorem euismod, et elementum ligula eleifend.
+        </div>
+        <div className="button-box">
+          <ColorButton variant="contained" color="primary" className='button' disableElevation onClick={click}>
+            Ver membros
+          </ColorButton>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Text
---
+++ site-minervas/src/components/who/Who.js
@@ -0,0 +1,28 @@
+import React, {useRef} from "react"
+
+import exampleimage from "../../assets/example-image.svg"
+import Text from "./Text"
+import TextField from '@material-ui/core/TextField';
+
+import "./Content.scss"
+
+function Who() {
+  const ref = useRef(null);
+
+  const handleClick = () => {
+    ref.current?.scrollIntoView({behavior: 'smooth'});
+  }
+  return (
+    <div>
+      <div className="who-content">
+        <Text click={() => handleClick()}/>
+        <img src={exampleimage} className="doctors" alt="doctors" />
+      </div>
+      {/* <div ref={ref}>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </div> */}
+    </div>
+  )
+}
+
+export default Who
