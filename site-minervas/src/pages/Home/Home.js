190e1add8aa88c02154e5a131a973dc018c11ddb
Ingrid Pacheco <iq.pacheco23@gmail.com>
2023-03-21 21:11:16 -0300

    feat: add new component to homepage

--- site-minervas/src/pages/Home/Home.js
+++ site-minervas/src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import React from "react"
 import Content from "../../components/content/Content"
 import Footer from "../../components/footer/Footer"
 import Header from "../../components/header/Header"
+import Who from "../../components/who/Who"
 
 import "./Home.scss"
 
@@ -11,6 +12,7 @@ function Home() {
     <div className="Home">
       <Header />
       <Content />
+      <Who />
       <Footer />
     </div>
   )
